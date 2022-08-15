7bda62b358d12b9bd25f1625b82f7437b552c5ec
mmm1616 <mikimaronmoka@gmail.com>
2022-08-15 11:07:42 +0000

    [ECサイト]商品詳細画面

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,20 +1,27 @@
 class Public::ItemsController < ApplicationController
   def index
     @genres = Genre.all
-    @genre = Genre.find(params[:genre_id])
 
     if params[:genre_id] == nil
     @items = Item.page(params[:page]).per(10)
     else
     items = Item.where(genre_id: params[:genre_id])
     @items = items.page(params[:page]).per(10)
+    @genre = Genre.find(params[:genre_id])
     end
 
   end
 
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
+  
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+  end
+  
+  
   
   private
     def item_params
@@ -23,5 +30,9 @@ class Public::ItemsController < ApplicationController
     
     def genre_params
     params.require(:genre).permit(:genre_id, :name)
+    end
+    
+    def cart_item_params
+      params.require(:cart_item).permit(:amount, :customer_id)
     end
 end
