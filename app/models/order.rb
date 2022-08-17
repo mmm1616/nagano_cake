493b36454300cfbd6d369ea6763283245b6e5380
mmm1616 <mikimaronmoka@gmail.com>
2022-08-17 02:27:36 +0000

    [ECサイト]注文情報入力画面

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,15 @@
 class Order < ApplicationRecord
+ belongs_to :customer
+ has_one :order_detail
+ 
  enum payment_method:{ "クレジットカード": 0, "銀行振込": 1}
  
  def full_name
          self.last_name + " " + self.first_name
  end
+ 
+ def full_address
+    self.postal_code + " " + self.address + " " + self.name
+ end
+ 
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,3 @@
 class OrderDetail < ApplicationRecord
+    has_one :order
 end
